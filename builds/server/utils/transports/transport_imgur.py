39754357e527b662947dec61fb71d7f1cf4db945
Und3rf10w <jrec93@gmail.com>
2018-01-28 18:43:28 -0500

    added functionality to transmit the tokens from the server

--- builds/server/utils/transports/transport_imgur.py
+++ builds/server/utils/transports/transport_imgur.py
@@ -1,12 +1,17 @@
 from imgurpython import ImgurClient
 import PIL
 from PIL import Image
-import io
+from cStringIO import StringIO
+from time import sleep
+from sys import exit
+import urlparse
 
 # YOU NEED TO GET A TOKEN FOR YOUR APPLICATION FIRST.=
 # SET UP YOUR ACCOUNT.
 
+
 # <START OF GHETTO CONFIG SECTION>
+USERNAME = ''
 client_id = ''
 client_secret = ''
 access_token = 
@@ -23,69 +28,85 @@ refresh_token =
 def sendTokens(tokens):
 	# Sends tokens in plain text. Eventually, I'd like to get it so I can
 	# just pass it to the encoder, but this works for a prototype
-	
+		
 	img = Image.new('RGB', (1920,1080), color = 'red')
 	pix = img.load()
 	token_list = list(bytearray(tokens))
 	for byte in token_list:
-		for x in range(img.size[1] - 1):			
-			# Insert a UPDATE_TOKENS header
-			if x == 0:
-				pix[x,x] = (1, 1, 1)
-				pass
+		for x in range(1, len(token_list) + 1):
+			byte = token_list[x-1]
 			# Write the byte to the blue value in the pixel
 			pix[x,x] = (pix[x,x][0], pix[x,x][1], byte)
-			x += 1
-			pass
+			x = x + 1
 		pass
-
+	# Insert a UPDATE_TOKENS header
+	pix[0,0] = (1, 1, 1)
 	# Upload image to new album
-	fields = {
-		'title' = "TK4U"
-		'privacy' = "public"
-	}
+	fields = {}
+	fields = { 'title': "TK4U", 'privacy': "public"}
 	album_object = client.create_album(fields)
+	fields.update({"id": album_object['id']})
 	print album_object #DEBUG
 
 	# Upload our image to the album
-	img_byte_array = io.BytesIO()
-	tokenimg = img.save(img_byte_array, FORMAT='JPG')
-	y = client.upload(image=img_byte_array.getvalue(), album=album_object['id'], name="tkns")
-	# y = client.album_add_images(album_object['id'], img)
+	img_byte_array = cStringIO.StringIO()
+	img.save(img_byte_array, format='JPEG')
+	y = client.upload(image=img_byte_array.getvalue(), config=fields)
 	print y #DEBUG
 
-	# Return the token's album id
-	return album_object['id']
+	# Return the token's album hash
+	return album_object['deletehash']
 
 
 def prepTransport():
-    global client
-    client = ImgurClient(client_id, client_secret)
-
-    # Auth to imgur
-    authorization_url = client.get_auth_url('token')
-
-    print "Go to the following URL: {0}".format(authorization_url)
-
-    try:
-    	token = raw_input("Insert the provided token: ")
-    except:
-    	token = input("Insert the provided token: ")
-
-    global credentials
-    credentials = client.authorize(token, 'token')
-    client.set_user_auth(credentials['access_token'], credentials['refresh_token'])
-
-    # Sending access and refresh token to client
-    data = str(credentials['access_token'] + "," + credentials['refresh_token'])
-    token_album_id = sendTokens(data)
-
-    # Loop to check and see if our token album has been deleted
-    #   if not, sleep 60 seconds and try again
-    while True:
+	global client
+	client = ImgurClient(client_id, client_secret)
 
 
-    return 0
+	# Auth to imgur
+	authorization_url = client.get_auth_url('token')
+
+	print "Go to the following URL: {0}".format(authorization_url)
+	try:
+		token_url = raw_input("Insert the url you were redirected to with all parameters: ")
+	except:
+		token_url = input("Insert the url you were redirected to with all parameters: ")
+
+	parsed = urlparse.urlparse(token_url)
+
+	access_token = urlparse.parse_qs(parsed.fragment)['access_token'][0]
+	refresh_token = urlparse.parse_qs(parsed.fragment)['refresh_token'][0]
+	client.set_user_auth(access_token, refresh_token)
+
+	if client.get_email_verification_status(USERNAME) == False:
+		print "ERROR: YOU NEED TO VERIFY YOUR EMAIL. We'll send a verification request."
+		client.send_verification_email(USERNAME)
+		print "Verification email sent. Exiting..."
+		exit(1)
+
+	# Sending access and refresh token to client
+	data = str(access_token + "," + refresh_token)
+	token_album_hash = sendTokens(data)
+
+	# Loop to check and see if our token album has been deleted
+	#   if not, sleep 60 seconds and try again
+	while True:
+		try: 
+			client.get_album(token_album_hash)
+			sleep(60)
+		# Couldn't find the album, need to set a general exception that tries again, but also need to find what kind
+		# of exception we'll get when the album no longer exists
+		
+
+		# except AlbumNotFoundError:
+		#	break
+
+		except Exception as e:
+			print "Caught unknown exception: %s" % (str(exception))
+
+
+	# Client has recieved the tokens
+	return 0
 
 
 def sendData(data):
