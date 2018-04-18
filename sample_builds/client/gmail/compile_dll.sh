04087483e6e3f62f19920db8899c86daf55f47e6
Und3rf10w <jrec93@gmail.com>
2018-04-18 18:45:58 -0400

    reorged project to prepare for rewrite

--- sample_builds/client/gmail/compile_dll.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-i686-w64-mingw32-gcc -shared c2file_dll.c -o c2file.dll
--- sample_builds/client/raw_socket/c2file_dll.h
+++
@@ -1,8 +0,0 @@
-#ifndef c2file_H__
-#define c2file_H__
-
-DWORD read_frame(HANDLE my_handle, char * buffer, DWORD max)
-void write_frame(HANDLE my_handle, char * buffer, DWORD length)
-HANDLE start_beacon(char * payload, DWORD length)
-
-#endif
